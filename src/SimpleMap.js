05e666fe8ff2469121e9e08459d9ff82ee23559f
Zaynaib Giwa <zaynaibg@gmail.com>
2018-09-24 19:12:39 -0500

    style/chore: changed map position to Chicago and added Marker componenet

--- src/SimpleMap.js
+++ src/SimpleMap.js
@@ -1,7 +1,5 @@
 import React , { Component }  from 'react'
-import { render } from 'react-dom'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
-import L from 'leaflet'
 
 class SimpleMap extends Component{
   componentDidMount(){
@@ -9,19 +7,19 @@ class SimpleMap extends Component{
     
   }
   render(){
-    const position = [51.505, -0.09]
+    const position = [41.8781, -87.6298]
     return(
       
-      <div id="map">
-  <Map center={position} zoom={13}>
-    <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-    />
-    <Marker position={position}>
-      <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
-    </Marker>
-  </Map>
+      <div className="map">
+    <Map center={position} zoom={13}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+      />
+      <Marker position={position}>
+        <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+      </Marker>
+    </Map>
 
     
      </div>
